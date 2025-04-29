c111c14986b6517cbbcf7b9cdf61c7ef59423bb0
elizinaaaa <elizacveigele730@gmail.com>
2025-04-29 11:20:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("poga")
+button.onclick = function() {
+const name = prompt ("Kā tevi sauc?")
+const age = prompt ("kāda ir tava mīļākā filma")
+alert ("Sveiki," + name + "! Paldies ka dalījies ar savu mīļāko filmu," + age + ".")
+}
+ 
